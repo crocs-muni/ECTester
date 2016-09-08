92be80192c90a9ebe9c821dbe24c940f4884c517
petrs <petrsgit@gmail.com>
2016-09-08 17:51:15 +0200

    added support for curves  secp128r1,  secp160r1, secp224r1

--- src/applets/EC_Consts.java
+++ src/applets/EC_Consts.java
@@ -20,6 +20,97 @@ public class EC_Consts {
     public static byte[] EC_FP_R = null;
     public static short EC_FP_K = 1;
     
+
+
+    // secp128r1 
+    public static final byte[] EC128_FP_P = new byte[]{
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFD,
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, 
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, 
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF};
+    
+    public static final byte[] EC128_FP_A = new byte[]{
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFD, 
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, 
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, 
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFC};
+    
+    public static final byte[] EC128_FP_B = new byte[]{
+        (byte) 0xE8, (byte) 0x75, (byte) 0x79, (byte) 0xC1,
+        (byte) 0x10, (byte) 0x79, (byte) 0xF4, (byte) 0x3D,
+        (byte) 0xD8, (byte) 0x24, (byte) 0x99, (byte) 0x3C,
+        (byte) 0x2C, (byte) 0xEE, (byte) 0x5E, (byte) 0xD3};
+    
+    // G in compressed form / first part of ucompressed
+    public static final byte[] EC128_FP_G_X = new byte[]{
+        (byte) 0x16, (byte) 0x1F, (byte) 0xF7, (byte) 0x52,
+        (byte) 0x8B, (byte) 0x89, (byte) 0x9B, (byte) 0x2D,
+        (byte) 0x0C, (byte) 0x28, (byte) 0x60, (byte) 0x7C,
+        (byte) 0xA5, (byte) 0x2C, (byte) 0x5B, (byte) 0x86 };
+    
+    // second part of G uncompressed
+    public static final byte[] EC128_FP_G_Y = new byte[]{
+        (byte) 0xCF, (byte) 0x5A, (byte) 0xC8, (byte) 0x39, 
+        (byte) 0x5B, (byte) 0xAF, (byte) 0xEB, (byte) 0x13,
+        (byte) 0xC0, (byte) 0x2D, (byte) 0xA2, (byte) 0x92,
+        (byte) 0xDD, (byte) 0xED, (byte) 0x7A, (byte) 0x83};
+    // Order of G
+    public static final byte[] EC128_FP_R = new byte[]{
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFE, 
+        (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, 
+        (byte) 0x75, (byte) 0xA3, (byte) 0x0D, (byte) 0x1B, 
+        (byte) 0x90, (byte) 0x38, (byte) 0xA1, (byte) 0x15};
+    // cofactor of G
+    public static final short EC128_FP_K = 1;
+    
+    // secp160r1 
+    public static final byte[] EC160_FP_P = new byte[]{
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, 
+        (byte) 0x7F, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF};
+
+    public static final byte[] EC160_FP_A = new byte[]{
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
+        (byte) 0x7F, (byte) 0xFF, (byte) 0xFF, (byte) 0xFC};
+
+    public static final byte[] EC160_FP_B = new byte[]{
+        (byte) 0x1C, (byte) 0x97, (byte) 0xBE, (byte) 0xFC, 
+        (byte) 0x54, (byte) 0xBD, (byte) 0x7A, (byte) 0x8B, 
+        (byte) 0x65, (byte) 0xAC, (byte) 0xF8, (byte) 0x9F, 
+        (byte) 0x81, (byte) 0xD4, (byte) 0xD4, (byte) 0xAD, 
+        (byte) 0xC5, (byte) 0x65, (byte) 0xFA, (byte) 0x45};
+
+    // G in compressed form / first part of ucompressed
+    public static final byte[] EC160_FP_G_X = new byte[]{
+        (byte) 0x4A, (byte) 0x96, (byte) 0xB5, (byte) 0x68,
+        (byte) 0x8E, (byte) 0xF5, (byte) 0x73, (byte) 0x28,
+        (byte) 0x46, (byte) 0x64, (byte) 0x69, (byte) 0x89,
+        (byte) 0x68, (byte) 0xC3, (byte) 0x8B, (byte) 0xB9,
+        (byte) 0x13, (byte) 0xCB, (byte) 0xFC, (byte) 0x82};
+
+    // second part of G uncompressed
+    public static final byte[] EC160_FP_G_Y = new byte[]{
+        (byte) 0x23, (byte) 0xA6, (byte) 0x28, (byte) 0x55, 
+        (byte) 0x31, (byte) 0x68, (byte) 0x94, (byte) 0x7D,
+        (byte) 0x59, (byte) 0xDC, (byte) 0xC9, (byte) 0x12, 
+        (byte) 0x04, (byte) 0x23, (byte) 0x51, (byte) 0x37, 
+        (byte) 0x7A, (byte) 0xC5, (byte) 0xFB, (byte) 0x32};
+    // Order of G
+    public static final byte[] EC160_FP_R = new byte[]{
+        (byte) 0x01, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, 
+        (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, 
+        (byte) 0x00, (byte) 0x01, (byte) 0xF4, (byte) 0xC8, 
+        (byte) 0xF9, (byte) 0x27, (byte) 0xAE, (byte) 0xD3, 
+        (byte) 0xCA, (byte) 0x75, (byte) 0x22, (byte) 0x57};
+    // cofactor of G
+    public static final short EC160_FP_K = 1;    
+    
+    
     // secp192r1 from http://www.secg.org/sec2-v2.pdf
     public static final byte[] EC192_FP_P = new byte[]{
         (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
@@ -68,6 +159,64 @@ public class EC_Consts {
         (byte) 0xB4, (byte) 0xD2, (byte) 0x28, (byte) 0x31};
     // cofactor of G
     public static final short EC192_FP_K = 1;    
+    
+    // secp224r1 from http://www.secg.org/sec2-v2.pdf
+    public static final byte[] EC224_FP_P = new byte[]{
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, 
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
+        (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+        (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+        (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x01};
+    
+    public static final byte[] EC224_FP_A = new byte[]{
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFE,
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFE};
+    
+    public static final byte[] EC224_FP_B = new byte[]{
+        (byte) 0xB4, (byte) 0x05, (byte) 0x0A, (byte) 0x85,
+        (byte) 0x0C, (byte) 0x04, (byte) 0xB3, (byte) 0xAB, 
+        (byte) 0xF5, (byte) 0x41, (byte) 0x32, (byte) 0x56, 
+        (byte) 0x50, (byte) 0x44, (byte) 0xB0, (byte) 0xB7, 
+        (byte) 0xD7, (byte) 0xBF, (byte) 0xD8, (byte) 0xBA, 
+        (byte) 0x27, (byte) 0x0B, (byte) 0x39, (byte) 0x43, 
+        (byte) 0x23, (byte) 0x55, (byte) 0xFF, (byte) 0xB4};
+    
+    // G in compressed form / first part of ucompressed
+    public static final byte[] EC224_FP_G_X = new byte[]{
+        (byte) 0xB7, (byte) 0x0E, (byte) 0x0C, (byte) 0xBD,
+        (byte) 0x6B, (byte) 0xB4, (byte) 0xBF, (byte) 0x7F,
+        (byte) 0x32, (byte) 0x13, (byte) 0x90, (byte) 0xB9, 
+        (byte) 0x4A, (byte) 0x03, (byte) 0xC1, (byte) 0xD3,
+        (byte) 0x56, (byte) 0xC2, (byte) 0x11, (byte) 0x22,
+        (byte) 0x34, (byte) 0x32, (byte) 0x80, (byte) 0xD6, 
+        (byte) 0x11, (byte) 0x5C, (byte) 0x1D, (byte) 0x21};
+    // second part of G uncompressed
+    public static final byte[] EC224_FP_G_Y = new byte[]{
+        (byte) 0xBD, (byte) 0x37, (byte) 0x63, (byte) 0x88, 
+        (byte) 0xB5, (byte) 0xF7, (byte) 0x23, (byte) 0xFB, 
+        (byte) 0x4C, (byte) 0x22, (byte) 0xDF, (byte) 0xE6, 
+        (byte) 0xCD, (byte) 0x43, (byte) 0x75, (byte) 0xA0,
+        (byte) 0x5A, (byte) 0x07, (byte) 0x47, (byte) 0x64, 
+        (byte) 0x44, (byte) 0xD5, (byte) 0x81, (byte) 0x99, 
+        (byte) 0x85, (byte) 0x00, (byte) 0x7E, (byte) 0x34};
+    // Order of G
+    public static final byte[] EC224_FP_R = new byte[]{
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, 
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
+        (byte) 0xFF, (byte) 0xFF, (byte) 0x16, (byte) 0xA2,
+        (byte) 0xE0, (byte) 0xB8, (byte) 0xF0, (byte) 0x3E,
+        (byte) 0x13, (byte) 0xDD, (byte) 0x29, (byte) 0x45,
+        (byte) 0x5C, (byte) 0x5C, (byte) 0x2A, (byte) 0x3D};
+    // cofactor of G
+    public static final short EC224_FP_K = 1;    
     
     // secp256r1 from http://www.secg.org/sec2-v2.pdf
     public static final byte[] EC256_FP_P = new byte[]{
@@ -142,6 +291,26 @@ public class EC_Consts {
         if (ecClass == KeyPair.ALG_EC_FP) {
             // Select proper courve parameters
             switch (ecLength) {
+                case (short) 128: {
+                    EC_FP_P = EC128_FP_P;
+                    EC_FP_A = EC128_FP_A;
+                    EC_FP_B = EC128_FP_B;
+                    EC_FP_G_X = EC128_FP_G_X;
+                    EC_FP_G_Y = EC128_FP_G_Y;
+                    EC_FP_R = EC128_FP_R;
+                    EC_FP_K = EC128_FP_K;
+                    break;
+                }          
+                case (short) 160: {
+                    EC_FP_P = EC160_FP_P;
+                    EC_FP_A = EC160_FP_A;
+                    EC_FP_B = EC160_FP_B;
+                    EC_FP_G_X = EC160_FP_G_X;
+                    EC_FP_G_Y = EC160_FP_G_Y;
+                    EC_FP_R = EC160_FP_R;
+                    EC_FP_K = EC160_FP_K;
+                    break;
+                }                
                 case (short) 192: {
                     EC_FP_P = EC192_FP_P;
                     EC_FP_A = EC192_FP_A;
@@ -152,6 +321,16 @@ public class EC_Consts {
                     EC_FP_K = EC192_FP_K;     
                     break;
                 }
+                case (short) 224: {
+                    EC_FP_P = EC224_FP_P;
+                    EC_FP_A = EC224_FP_A;
+                    EC_FP_B = EC224_FP_B;
+                    EC_FP_G_X = EC224_FP_G_X;
+                    EC_FP_G_Y = EC224_FP_G_Y;
+                    EC_FP_R = EC224_FP_R;
+                    EC_FP_K = EC224_FP_K;
+                    break;
+                }                
                 case (short) 256: {
                     EC_FP_P = EC256_FP_P;
                     EC_FP_A = EC256_FP_A;
@@ -180,7 +359,7 @@ public class EC_Consts {
             ecPubKey.setFieldFP(EC_FP_P, (short) 0, (short) EC_FP_P.length);
             ecPubKey.setA(EC_FP_A, (short) 0, (short) EC_FP_A.length);
             ecPubKey.setB(EC_FP_B, (short) 0, (short) EC_FP_B.length);
-            if (bInvalidCurve) { // corrupt curve
+            if (bInvalidCurve) { // corrupt curve if required for testing
                 Util.arrayCopyNonAtomic(EC_FP_B, (short) 0, auxBuffer, (short) 0, (short) EC_FP_B.length);                
                 auxBuffer[(byte) 10] = (byte) 0xcc;
                 auxBuffer[(byte) 11] = (byte) 0xcc;
