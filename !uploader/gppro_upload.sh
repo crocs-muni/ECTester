0ae3b300c51a9c6bbde103076f089428bbe3e55e
J08nY <johny@neuromancer.sk>
2016-10-24 19:04:25 +0200

    fixed ant build on linux, added utility scripts in bash

---
+++ !uploader/gppro_upload.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+java -jar gp.jar -delete 4543546573746572 -deletedeps -verbose
+java -jar gp.jar -deletedeps -verbose -delete 4A43416C6754657374
+
+java -jar gp.jar -install simpleECC.cap -verbose
+
+
