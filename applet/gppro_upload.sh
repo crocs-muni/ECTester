aeee92d6a463a7bc2886f653b771ea60ffa0dd1d
J08nY <johny@neuromancer.sk>
2018-04-22 00:16:18 +0200

    Cleanup gppro upload scripts.

--- applet/gppro_upload.sh
+++ applet/gppro_upload.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-java -jar gp.jar -delete 4543546573746572 -deletedeps -verbose
+java -jar gp.jar -deletedeps -verbose -delete 4C6162616B417070
 java -jar gp.jar -deletedeps -verbose -delete 4A43416C6754657374
+java -jar gp.jar -deletedeps -verbose -delete 4543546573746572
 
-java -jar gp.jar -install ectester.cap -verbose
+java -jar gp.jar -install ectester.cap -verbose -d
 
 
