05b00a3e98dff32f4fd5af51425c95144e09d65b
davidhofman <davidhofman1996@gmail.com>
2021-09-01 20:14:56 +0200

    Implement StandaloneWrongSuite

--- src/cz/crcs/ectester/standalone/test/suites/StandaloneWrongSuite.java
+++ src/cz/crcs/ectester/standalone/test/suites/StandaloneWrongSuite.java
@@ -1,12 +1,14 @@
 package cz.crcs.ectester.standalone.test.suites;
 
+import cz.crcs.ectester.applet.EC_Consts;
 import cz.crcs.ectester.common.cli.TreeCommandLine;
 import cz.crcs.ectester.common.ec.EC_Curve;
 import cz.crcs.ectester.common.output.TestWriter;
 import cz.crcs.ectester.common.test.CompoundTest;
 import cz.crcs.ectester.common.test.Result;
 import cz.crcs.ectester.common.test.Test;
-import cz.crcs.ectester.common.util.CardUtil;
+import cz.crcs.ectester.common.util.ByteUtil;
+import cz.crcs.ectester.common.util.ECUtil;
 import cz.crcs.ectester.data.EC_Store;
 import cz.crcs.ectester.standalone.ECTesterStandalone;
 import cz.crcs.ectester.standalone.consts.KeyAgreementIdent;
@@ -17,26 +19,26 @@ import cz.crcs.ectester.standalone.test.base.KeyGeneratorTest;
 import cz.crcs.ectester.standalone.test.base.KeyGeneratorTestable;
 
 import javax.crypto.KeyAgreement;
+import java.math.BigInteger;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.interfaces.ECPrivateKey;
 import java.security.interfaces.ECPublicKey;
 import java.security.spec.ECParameterSpec;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class StandaloneWrongSuite extends StandaloneTestSuite {
     private String kpgAlgo;
     private String kaAlgo;
-    private String sigAlgo;
-    private List<String> kaTypes;
-    private List<String> sigTypes;
+    KeyAgreement ka;
+    KeyPairGenerator kpg;
 
     public StandaloneWrongSuite(TestWriter writer, ECTesterStandalone.Config cfg, TreeCommandLine cli) {
         super(writer, cfg, cli, "wrong", "The wrong curve suite tests whether the library rejects domain parameters which are not curves.",
                 "Supports options:",
                 "\t - gt/kpg-type",
-                "\t - kt/ka-type (select multiple types by separating them with commas)",
-                "\t - st/sig-type (select multiple types by separating them with commas)");
+                "\t - kt/ka-type");
     }
 
 
@@ -44,9 +46,6 @@ public class StandaloneWrongSuite extends StandaloneTestSuite {
     protected void runTests() throws Exception {
         kpgAlgo = cli.getOptionValue("test.kpg-type");
         kaAlgo = cli.getOptionValue("test.ka-type");
-        sigAlgo = cli.getOptionValue("test.sig-type");
-        kaTypes = kaAlgo != null ? Arrays.asList(kaAlgo.split(",")) : new ArrayList<>();
-        sigTypes = sigAlgo != null ? Arrays.asList(sigAlgo.split(",")) : new ArrayList<>();
 
         KeyPairGeneratorIdent kpgIdent;
         if (kpgAlgo == null) {
@@ -72,13 +71,40 @@ public class StandaloneWrongSuite extends StandaloneTestSuite {
                 return;
             }
         }
-        KeyPairGenerator kpg = kpgIdent.getInstance(cfg.selected.getProvider());
+        kpg = kpgIdent.getInstance(cfg.selected.getProvider());
+
+        KeyAgreementIdent kaIdent;
+        if (kaAlgo == null) {
+            // try ECDH, if not, fail with: need to specify ka algo.
+            Optional<KeyAgreementIdent> kaIdentOpt = cfg.selected.getKAs().stream()
+                    .filter((ident) -> ident.contains("ECDH"))
+                    .findFirst();
+            if (kaIdentOpt.isPresent()) {
+                kaIdent = kaIdentOpt.get();
+            } else {
+                System.err.println("The default KeyAgreement algorithm type of \"ECDH\" was not found. Need to specify a type.");
+                return;
+            }
+        } else {
+            // try the specified, if not, fail with: wrong ka algo/not found.
+            Optional<KeyAgreementIdent> kaIdentOpt = cfg.selected.getKAs().stream()
+                    .filter((ident) -> ident.contains(kaAlgo))
+                    .findFirst();
+            if (kaIdentOpt.isPresent()) {
+                kaIdent = kaIdentOpt.get();
+            } else {
+                System.err.println("The KeyAgreement algorithm type of \"" + kaAlgo + "\" was not found.");
+                return;
+            }
+        }
+        ka = kaIdent.getInstance(cfg.selected.getProvider());
 
         /* Just do the default run on the wrong curves.
          * These should generally fail, the curves aren't curves.
          */
-        Map<String, EC_Curve> curves = EC_Store.getInstance().getObjects(EC_Curve.class, "wrong");
-        for (Map.Entry<String, EC_Curve> e : curves.entrySet()) {
+        Map<String, EC_Curve> wrongCurves = EC_Store.getInstance().getObjects(EC_Curve.class, "wrong");
+        for (Map.Entry<String, EC_Curve> e : wrongCurves.entrySet()) {
+            /*
             EC_Curve curve = e.getValue();
             ECParameterSpec spec = curve.toSpec();
 
@@ -96,20 +122,172 @@ public class StandaloneWrongSuite extends StandaloneTestSuite {
             ECPrivateKey ecpriv = (ECPrivateKey) kp.getPrivate();
             ECPublicKey ecpub = (ECPublicKey) kp.getPublic();
 
-            //try all the specified kas
-            List<Test> kaTests = new LinkedList<>();
-            for (KeyAgreementIdent kaIdent : cfg.selected.getKAs()) {
-                if (kaAlgo == null || kaIdent.containsAny(kaTypes)) {
-                    KeyAgreement ka = kaIdent.getInstance(cfg.selected.getProvider());
-                    KeyAgreementTestable testable = new KeyAgreementTestable(ka, ecpriv, ecpub);
-                    kaTests.add(KeyAgreementTest.expectError(testable, Result.ExpectedValue.FAILURE));
-                }
-            }
-            if(kaTests.isEmpty()) {
-                kaTests.add(CompoundTest.all(Result.ExpectedValue.SUCCESS, "None of the specified KeyAgreement types is supported by the library."));
-            }
-            Test performKeyAgreements = CompoundTest.all(Result.ExpectedValue.SUCCESS, "Perform specified KeyAgreements.", kaTests.toArray(new Test[0]));
-            doTest(CompoundTest.all(Result.ExpectedValue.SUCCESS, "Wrong curve test of " + curve.getBits() + "b " + CardUtil.getKeyTypeString(curve.getField()), generateSuccess, performKeyAgreements));
+            KeyAgreement ka = kaIdent.getInstance(cfg.selected.getProvider());
+            KeyAgreementTestable testable = new KeyAgreementTestable(ka, ecpriv, ecpub);
+            Test ecdh = KeyAgreementTest.expectError(testable, Result.ExpectedValue.FAILURE);
+            doTest(CompoundTest.all(Result.ExpectedValue.SUCCESS, "Wrong curve test of " + curve.getBits() + "b " + CardUtil.getKeyTypeString(curve.getField()), generateSuccess, ecdh));
+             */
         }
+
+        /*
+         * Do some interesting tests with corrupting the custom curves.
+         * For prime field:
+         *  - p = 0
+         *  - p = 1
+         *  - p is a square of a prime
+         *  - p is a composite q * s with q, s primes
+         *  - TODO: p divides discriminant
+         */
+        Map<String, EC_Curve> curveMap = EC_Store.getInstance().getObjects(EC_Curve.class, "secg");
+        List<EC_Curve> curves = curveMap.entrySet().stream().filter((e) -> e.getKey().endsWith("r1") &&
+                e.getValue().getField() == javacard.security.KeyPair.ALG_EC_FP).map(Map.Entry::getValue).collect(Collectors.toList());
+        Random r = new Random();
+        for (EC_Curve curve : curves) {
+            short bits = curve.getBits();
+            byte[][] originalp = curve.getParam(EC_Consts.PARAMETER_FP);
+
+            curve.setParam(EC_Consts.PARAMETER_FP, new byte[][]{ ByteUtil.hexToBytes("0")});
+            //Test prime0 = ecdhTest(curve.toSpec(),"ECDH with p = 0.");
+
+            curve.setParam(EC_Consts.PARAMETER_FP, new byte[][]{ ByteUtil.hexToBytes("1")});
+            //Test prime1 = ecdhTest(curve.toSpec(),"ECDH with p = 1.");
+
+            short keyHalf = (short) (bits / 2);
+            BigInteger prime = new BigInteger(keyHalf, 50, r);
+            BigInteger primePow = prime.pow(2);
+            byte[] primePowBytes = ECUtil.toByteArray(primePow, bits);
+            curve.setParam(EC_Consts.PARAMETER_FP, new byte[][]{primePowBytes});
+
+            //Test primePower = ecdhTest(curve.toSpec(), "ECDH with p = q^2.");
+
+            BigInteger q = new BigInteger(keyHalf, r);
+            BigInteger s = new BigInteger(keyHalf, r);
+            BigInteger compositeValue = q.multiply(s);
+            byte[] compositeBytes = ECUtil.toByteArray(compositeValue, bits);
+            curve.setParam(EC_Consts.PARAMETER_FP, new byte[][]{compositeBytes});
+
+            //Test composite = ecdhTest(curve.toSpec(), "ECDH with p = q * s.");
+
+            //Test wrongPrime = CompoundTest.all(Result.ExpectedValue.SUCCESS, "Tests with corrupted prime parameter.", /* prime0, */ /* prime1, */ /* primePower, */ /* composite */ );
+
+            curve.setParam(EC_Consts.PARAMETER_FP, originalp);
+            byte[][] originalG = curve.getParam(EC_Consts.PARAMETER_G);
+
+            //curve.setParam(EC_Consts.PARAMETER_G, ??);
+            //Test randomG = ecdhTest(curve.toSpec(), "ECDH with non-point G.");
+
+            byte[] Gx = new BigInteger(curve.getBits(), r).toByteArray();
+            byte[] Gy = new BigInteger(curve.getBits(), r).toByteArray();
+            curve.setParam(EC_Consts.PARAMETER_G, new byte[][] {Gx, Gy});
+            Test fullRandomG = ecdhTest(curve.toSpec(), "ECDH with G = random data.");
+
+            curve.setParam(EC_Consts.PARAMETER_G, new byte[][] {ByteUtil.hexToBytes("0"), ByteUtil.hexToBytes("0")});
+            Test zeroG = ecdhTest(curve.toSpec(), "ECDH with G = infinity.");
+
+            Test wrongG = CompoundTest.all(Result.ExpectedValue.SUCCESS, "Tests with corrupted G parameter.", /* randomG, */ fullRandomG, zeroG);
+
+            curve.setParam(EC_Consts.PARAMETER_G, originalG);
+            byte[] originalR = curve.getParam(EC_Consts.PARAMETER_R)[0];
+            BigInteger originalBigR = new BigInteger(1, originalR);
+
+            byte[] RZero = new byte[]{(byte) 0};
+            curve.setParam(EC_Consts.PARAMETER_R, new byte[][] {RZero});
+            //Test zeroR = ecdhTest(curve.toSpec(), "ECDH with R = 0.");
+
+            byte[] ROne = new byte[]{(byte) 1};
+            curve.setParam(EC_Consts.PARAMETER_R, new byte[][] {ROne});
+            //Test oneR = ecdhTest(curve.toSpec(),  "ECDH with R = 1.");
+
+
+            BigInteger prevPrimeR;
+            do {
+                prevPrimeR = BigInteger.probablePrime(originalBigR.bitLength() - 1, r);
+            } while (prevPrimeR.compareTo(originalBigR) >= 0);
+            byte[] prevRBytes = ECUtil.toByteArray(prevPrimeR, bits);
+            curve.setParam(EC_Consts.PARAMETER_R, new byte[][] {prevRBytes});
+            Test prevprimeWrongR = ecdhTest(curve.toSpec(),  "ECDH with R = some prime (but [r]G != infinity) smaller than original R.");
+
+
+            BigInteger nextPrimeR = originalBigR.nextProbablePrime();
+            byte[] nextRBytes = ECUtil.toByteArray(nextPrimeR, bits);
+            curve.setParam(EC_Consts.PARAMETER_R, new byte[][]{nextRBytes});
+            Test nextprimeWrongR = ecdhTest(curve.toSpec(), "ECDH with R = some prime (but [r]G != infinity) larger than original R.");
+
+            byte[] nonprimeRBytes = nextRBytes.clone();
+            nonprimeRBytes[nonprimeRBytes.length - 1] ^= 1;
+            curve.setParam(EC_Consts.PARAMETER_R, new byte[][] {nonprimeRBytes} );
+            Test nonprimeWrongR = ecdhTest(curve.toSpec(), "ECDH with R = some composite (but [r]G != infinity).");
+
+            Test wrongR = CompoundTest.all(Result.ExpectedValue.SUCCESS, "Tests with corrupted R parameter.", /* zeroR, */ /* oneR, */ prevprimeWrongR, nextprimeWrongR, nonprimeWrongR);
+
+            curve.setParam(EC_Consts.PARAMETER_R, new byte[][] {originalR});
+
+            byte[] kRaw = new byte[]{(byte) 0xff};
+            curve.setParam(EC_Consts.PARAMETER_K, new byte[][] {kRaw});
+            //Test bigK = ecdhTest(curve.toSpec(), "ECDH with big K.");
+
+            byte[] kZero = new byte[]{(byte) 0};
+            curve.setParam(EC_Consts.PARAMETER_K, new byte[][]{kZero});
+            //Test zeroK = ecdhTest(curve.toSpec(), "ECDH with K = 0.");
+
+            //Test wrongK = CompoundTest.all(Result.ExpectedValue.SUCCESS, "Tests with corrupted K parameter.", /* bigK, */, /* zeroK */);
+
+            doTest(CompoundTest.all(Result.ExpectedValue.SUCCESS, "Tests of " + bits + "b " + "FP", /* wrongPrime, */ wrongG, wrongR /*, wrongK */));
+
+        }
+
+
+        /*
+         * For binary field:
+         *  - e1 = e2 = e3 = 0
+         *  - e1, e2 or e3 is larger than m.
+         */
+        curveMap = EC_Store.getInstance().getObjects(EC_Curve.class, "secg");
+        curves = curveMap.entrySet().stream().filter((e) -> e.getKey().endsWith("r1") &&
+                e.getValue().getField() == javacard.security.KeyPair.ALG_EC_F2M).map(Map.Entry::getValue).collect(Collectors.toList());
+        for (EC_Curve curve : curves) {
+            short bits = curve.getBits();
+            byte[][] coeffBytes;
+
+
+            coeffBytes = new byte[][]{
+                    ByteUtil.shortToBytes(bits),
+                    ByteUtil.shortToBytes((short) 0),
+                    ByteUtil.shortToBytes((short) 0),
+                    ByteUtil.shortToBytes((short) 0)};
+            curve.setParam(EC_Consts.PARAMETER_F2M, coeffBytes);
+            //Test coeff0 = ecdhTest(curve.toSpec(), "ECDH with wrong field polynomial: x^");
+
+            short e1 = (short) (2 * bits);
+            short e2 = (short) (3 * bits);
+            short e3 = (short) (4 * bits);
+            coeffBytes = new byte[][]{
+                    ByteUtil.shortToBytes(bits),
+                    ByteUtil.shortToBytes(e1),
+                    ByteUtil.shortToBytes(e2),
+                    ByteUtil.shortToBytes(e3)};
+            curve.setParam(EC_Consts.PARAMETER_F2M, coeffBytes);
+            //Test coeffLarger = ecdhTest(curve.toSpec(), "ECDH with wrong field poly, powers larger than " + bits);
+
+            //doTest(CompoundTest.all(Result.ExpectedValue.SUCCESS, "Tests with corrupted field polynomial parameter over " + curve.getBits() + "b F2M",/* coeff0, */ /* coeffLarger */));
+        }
+    }
+
+    private Test ecdhTest(ECParameterSpec spec, String desc) {
+        //generate KeyPair
+        KeyGeneratorTestable kgt = new KeyGeneratorTestable(kpg, spec);
+        Test generate =  KeyGeneratorTest.expectError(kgt, Result.ExpectedValue.FAILURE);
+        runTest(generate);
+        KeyPair kp = kgt.getKeyPair();
+        if(kp == null) {
+            return CompoundTest.all(Result.ExpectedValue.SUCCESS, desc, generate);
+        }
+        ECPublicKey pub = (ECPublicKey) kp.getPublic();
+        ECPrivateKey priv = (ECPrivateKey) kp.getPrivate();
+
+        //perform ECDH
+        KeyAgreementTestable testable = new KeyAgreementTestable(ka, priv, pub);
+        Test ecdh = KeyAgreementTest.expect(testable, Result.ExpectedValue.FAILURE);
+        return CompoundTest.all(Result.ExpectedValue.SUCCESS, desc, generate, ecdh);
     }
 }
