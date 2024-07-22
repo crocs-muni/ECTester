af299ba4547319a2bcd6a0e035e2fd5cfadd47a7
quapka <quapka@gmail.com>
2024-07-22 13:38:29 +0200

    Remove unnecessary dependency since MbedTLS package is now used

--- standalone/src/main/java/cz/crcs/ectester/standalone/libs/MbedTLSLib.java
+++ standalone/src/main/java/cz/crcs/ectester/standalone/libs/MbedTLSLib.java
@@ -9,7 +9,7 @@ import java.util.Set;
 public class MbedTLSLib extends NativeECLibrary {
 
     public MbedTLSLib() {
-        super("mbedTLS", "mbedtls_provider", "lib_mbedtls.so");
+        super("mbedTLS", "mbedtls_provider");
     }
 
     @Override
