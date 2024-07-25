c7300fb4a772b0d2364b0fff20f9676fdb79ac9b
quapka <quapka@gmail.com>
2024-07-25 10:11:36 +0200

    Link Crypto++ statically

--- flake.nix
+++ flake.nix
@@ -66,6 +66,7 @@
         libtomcrypt = pkgs.libtomcrypt.overrideAttrs (final: prev: {
           makefile = "makefile.unix";
         });
+        cryptopp = pkgs.cryptopp.override { enableStatic = true; };
         libressl = pkgs.libressl.overrideAttrs (_old: rec {
           # devLibPath = pkgs.lib.makeLibraryPath [ pkgs.libressl.dev ];
           # pname = "libressl";
