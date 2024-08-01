b3fe06d3f2dcae7462552dc8b1cf94ab4747cbdc
J08nY <johny@neuromancer.sk>
2024-08-01 16:23:07 +0200

    Add more reader tests.

--- reader/src/test/java/cz/crcs/ectester/reader/AppTests.java
+++ reader/src/test/java/cz/crcs/ectester/reader/AppTests.java
@@ -108,4 +108,29 @@ public class AppTests {
     public void miscellaneousSuite() {
         assertTimeoutPreemptively(Duration.ofSeconds(60), () -> ECTesterReader.main(new String[]{"-t", "miscellaneous", "-s", "-y"}));
     }
+
+    @Test
+    public void generate() {
+        ECTesterReader.main(new String[]{"-g", "10", "-s", "-fp", "-o", "/dev/null", "-b", "256"});
+    }
+
+    @Test
+    public void ecdh() {
+        ECTesterReader.main(new String[]{"-dh", "-fp", "-b", "256", "-s"});
+    }
+
+    @Test
+    public void ecdsa() {
+        ECTesterReader.main(new String[]{"-dsa", "-fp", "-b", "256", "-s"});
+    }
+
+    @Test
+    public void export() {
+        ECTesterReader.main(new String[]{"-e", "-fp", "-b", "256", "-s", "-o", "/dev/null"});
+    }
+
+    @Test
+    public void info() {
+        ECTesterReader.main(new String[]{"-nf", "-s"});
+    }
 }
