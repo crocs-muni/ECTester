fcca3a431eed737dc970ee76076e77b0e44538ce
J08nY <johny@neuromancer.sk>
2025-03-22 22:17:34 +0100

    Fix ECDH external in reader.

--- reader/src/test/java/cz/crcs/ectester/reader/OutputTests.java
+++ reader/src/test/java/cz/crcs/ectester/reader/OutputTests.java
@@ -1,13 +1,20 @@
 package cz.crcs.ectester.reader;
 
+import org.junit.jupiter.api.io.TempDir;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.nio.file.Path;
+
 public class OutputTests {
+
+    @TempDir
+    static Path tempDir;
 
     @ParameterizedTest
     @ValueSource(strings = {"text", "xml", "yml"})
     public void formats(String format) {
-        ECTesterReader.main(new String[]{"-t", "default", "-s", "-o", String.format("%s:out.%s", format, format),});
+        Path outputFile = tempDir.resolve(String.format("out.%s", format));
+        ECTesterReader.main(new String[]{"-t", "default", "-s", "-o", String.format("%s:%s", format, outputFile),});
     }
 }
